aa54f8c440eb9b4e3c5d6767811e18cc3d14b427
Jeotique <71186872+Jeotique@users.noreply.github.com>
2023-12-03 00:51:52 +0100

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -7,6 +7,10 @@ module.exports = {
         description: "Répond pong",
         type: Discord.commandType.CHAT_INPUT
     }),
+    /**
+    * @param {Bot} client
+    * @param {Discord.Interaction} interaction
+    */
     run: async(client, interaction) => {
         return interaction.reply("pong")
     }
