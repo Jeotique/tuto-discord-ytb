2c107ce82358362aac3a1c39dd717b5bcb422407
Jeotique <71186872+Jeotique@users.noreply.github.com>
2023-12-03 00:29:01 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -9,7 +9,11 @@ module.exports = {
     run: async(client) => {
         console.log(`${client.user.tag} est maintenant connecté`)
         console.log(`${client.guilds.size} serveur${client.guilds.size < 1 ? '':'s'}`)
-        client.guilds.map(guild => {
+            client.guilds.map(async guild => {
+            let commands = await guild.getCommands()
+            for (const command of commands) {
+                await guild.deleteCommand(command)
+            }
             guild.setCommands(client.commands.map(cmd => cmd.data))
         })
     }
